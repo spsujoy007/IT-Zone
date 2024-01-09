b871c530a61a6aef6493544ab45b9647fdcdc18b
Sujoy Paul <freelancersujoy@gmail.com>
2024-01-10 01:09:22 +0600

    recommendation part

--- JS/courses_handle.js
+++ JS/courses_handle.js
@@ -1,5 +1,6 @@
 let courseDiv = document.getElementById("coursesFather");
- 
+
+// here you can add new course or change course contents;
 const courses =  [
     {
         image:"../assets/course_img/pic1.jpg",
@@ -20,19 +21,19 @@ const courses =  [
 ]
 
 courseDiv.innerHTML = courses.map(cs => `
-        <div class="flex space-x-5 w-1/2 bg-slate-100 min-h-[100px]">
-        <div class="w-[745px] overflow-x-hidden">
-            <img class="" src=${cs.image} alt="">
+        <div class="flex w-[48%] hover:bg-slate-100 min-h-[100px] rounded-lg overflow-hidden border-[1px] border-slate-200  duration-300 hover:shadow-lg hover:shadow-slate-300 cursor-pointer">
+        <div class="w-[800px] overflow-hidden p-4 ">
+            <img class="rounded-xl" src=${cs.image} alt="">
         </div>
-        <div class="py-5">
-        <div class="">
-                <p class="bg-offprimary inline px-3 py-1 rounded-full primary font-bold text-sm">${cs.tag}</p>
-                <h4 class="text-xl font-bold mt-2">${cs.title}</h4>
-                <p class="text-sm mt-2">${cs.details}</p>
-        </div>
+        <div class="py-5 pr-5 h-full flex flex-col justify-between">
+            <div class="">
+                    <p class="bg-offprimary inline px-3 py-[2px] rounded-full primary font-bold text-sm">${cs.tag}</p>
+                    <h4 class="text-lg font-bold mt-2">${cs.title}</h4>
+                    <p class="text-xs mt-2">${cs.details}</p>
+            </div>
 
             <!-- admin info  -->
-            <div class="mt-10">
+            <div class="">
                 <div class="flex items-center space-x-3">
                     <img class="w-[40px] h-[40px] rounded-full border_primary " src="../assets/razaunur_ullah.png" alt="">
                     <div>
@@ -40,7 +41,7 @@ courseDiv.innerHTML = courses.map(cs => `
                         <p class="text-md">It Instructor</p>
                     </div>
                 </div>
-                <div class="mt-4 flex justify-between border-t-[1px] border-black pt-2 pr-4">
+                <div class="mt-4 flex justify-between border-t-[1px] border-black pt-2 ">
                     <p class="text-xl">৳ ${cs.price}<span class="primary"> BDT</span> </p>
                     <p class="flex gap-2 items-center"><i class="fa-solid fa-user-check"></i>${cs.purchased}</p>
                 </div>
